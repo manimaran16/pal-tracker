dce570a3340c016d081113155f184d0488fb0489
Priya <priya.laurel@gmail.com>
2020-03-21 15:32:10 -0400

    changes

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -1,12 +1,18 @@
 package io.pivotal.pal.tracker;
 
 import com.mysql.cj.jdbc.MysqlDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import javax.sql.DataSource;
+
 @SpringBootApplication
 public class PalTrackerApplication {
+    @Autowired
+    private DataSource dataSource;
+
     public static void main (String[] args )
     {
         SpringApplication.run(PalTrackerApplication.class,args);
